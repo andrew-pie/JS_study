dc02b723bd227943b10a6671a03131ea04e47395
Andrii Pyrozhok <pyrozhok.dev@gmail.com>
2023-01-24 16:28:34 +0800

    if else lesson 2

--- ifElse.js
+++ ifElse.js
@@ -1,5 +1,36 @@
 "use strict";
 
-if (4 == 4) {
+if (4 == 9) {
 	console.log("Ok!");
+} else {
+	console.log("Error");
+}
+
+const num = 50;
+
+// if (num < 49) {
+// 	console.log("Error");
+// } else if (num > 100) {
+// 	console.log("Много");
+// } else {
+// 	console.log("Ок!");
+// }
+
+// //If else 2 variant with ternar operator
+// (num === 50) ? console.log("Ок!") : console.log("Error");
+
+//Строгое сравнение
+switch (num) {
+	case 49:
+		console.log("Неверно");
+		break;
+	case 100:
+		console.log("Неверно");
+		break;
+	case 50:
+		console.log("Верно!");
+		break;
+	default:
+		console.log("Не в этот раз")
+		break;
 }
