bc85df781ae9e70956fa105acc037c5a8088ae8d
Andrii Pyrozhok <pyrozhok.dev@gmail.com>
2023-01-27 09:58:25 +0800

    learn cycles and functions

--- cycle.js
+++ cycle.js
@@ -97,7 +97,7 @@ for (i = 5; i <= 10; i++) {
 }
 console.log(arrayOfNumbers);
 
-
+//rewrite array 
 // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
 const arr = [3, 5, 8, 16, 20, 23, 50];
 const result = [];
@@ -109,3 +109,64 @@ for (let i = 0; i < arr.length; i++) {
 }
 console.log(result);
 
+
+//Rename array elements
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+for (let i = 0; i < data.length; i++) {
+	console.log(typeof (data[i]));
+	if (typeof (data[i]) === "number") {
+		data[i] = data[i] * 2;
+	} else if (typeof (data[i]) === "string") {
+		data[i] = data[i] + " - done"
+	}
+}
+
+for (i = 0; i < data.length; i++) {
+	console.log(data[i])
+}
+
+
+// Разверните массив data наоборот при помощи цикла и запишите данные в массив result.
+// Должно получиться: [ 'Homework', 20, 'Shopping', 10, 5 ]
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const result = [];
+
+let j = 0;
+for (let i = data.length - 1; i >= 0; i--) {
+	result[j] = data[i];
+	j = j + 1;
+}
+
+for (i = 0; i < result.length; i++) {
+	console.log(result[i])
+}
+
+// Create
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
+
+const lines = 5;
+let result = '';
+// Проверяется именно переменная result, формируйте строку в ней
+
+
+for (let i = 0; i <= lines; i++) {
+
+	for (let j = 0; j < lines - i; j++) {
+		result += " ";
+	}
+
+	for (let j = 0; j < 2 * i + 1; j++) {
+		result += "*";
+	}
+
+	result += "\n";
+
+}
+
+console.log(result);
---
+++ function.js
@@ -0,0 +1,46 @@
+"use strict";
+
+let num = 20;
+
+//создать функцию с именем showFirstMessage
+function showFirstMessage(text) {
+	console.log(text);
+	let num = 10;
+	console.log(num);
+}
+
+//вызвать функцию
+showFirstMessage("Hello world");
+console.log(num);
+
+// function calc(a, b) {
+// 	return (a + b);
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+
+function ret() {
+	let num = 50;
+
+	//
+
+
+	return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+
+const logger = function () {
+	console.log("hello");
+};
+
+logger();
+
+//стрелочная функция
+const calc = (a, b) => { return a + b };
+
