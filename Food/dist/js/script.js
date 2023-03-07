f19fca63fc7b92a37c791febed12c83bdc0d01e5
Andrii Pyrozhok <pyrozhok.dev@gmail.com>
2023-03-07 10:53:20 +0700

    Add modal events esc and click, and open modal at bottom

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -129,126 +129,59 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	// Получем елементы со страницы
 	const modal = document.querySelector('.modal');
-	const openModalBtn = document.querySelector('[data-modal]');
+	const openModalBtn = document.querySelectorAll('[data-modal]');
 	const closeModalBtn = document.querySelector('[data-close]');
 
-
 	//открываем окно
-	const openModal = function () {
+	function openModal() {
 		modal.classList.add('show');
 		modal.classList.remove('hide');
-	};
-
-	openModalBtn.addEventListener('click', openModal);
+		// // 2 вариант
+		// modal.classList.toggle('show');
+		document.body.style.overflow = 'hidden';
+		clearInterval(modalTimerId);
+	}
 
 	// закрываем окно
-	const closeModal = function () {
+	function closeModal() {
 		modal.classList.add('hide');
 		modal.classList.remove('show');
-	};
+		// // 2 вариант
+		// modal.classList.toggle('hide');
+		document.body.style.overflow = '';
+	}
+
+	// Привязать кнопку к нескольким елементам
+	openModalBtn.forEach(btn => {
+		btn.addEventListener('click', openModal);
+	});
 
 	closeModalBtn.addEventListener('click', closeModal);
+
+	// close modal window on click in free space
+	modal.addEventListener('click', (e) => {
+		if (e.target === modal) {
+			closeModal();
+		}
+	});
+
+	// close modal with ESC button
+	document.addEventListener('keydown', (e) => {
+		if (e.code === 'Escape' && modal.classList.contains('show')) {
+			closeModal();
+		}
+	});
+
+	// open modal after 5 sec
+	const modalTimerId = setTimeout(openModal, 5000);
+
+	function showModalByScroll() {
+		if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+			openModal();
+			window.removeEventListener('scroll', showModalByScroll);
+		}
+	}
+
+	//open modal at page bottom
+	window.addEventListener('scroll', showModalByScroll);
 });
-
-
-// /******/ (function(modules) { // webpackBootstrap
-// /******/ 	// The module cache
-// /******/ 	var installedModules = {};
-// /******/
-// /******/ 	// The require function
-// /******/ 	function __webpack_require__(moduleId) {
-// /******/
-// /******/ 		// Check if module is in cache
-// /******/ 		if(installedModules[moduleId]) {
-// /******/ 			return installedModules[moduleId].exports;
-// /******/ 		}
-// /******/ 		// Create a new module (and put it into the cache)
-// /******/ 		var module = installedModules[moduleId] = {
-// /******/ 			i: moduleId,
-// /******/ 			l: false,
-// /******/ 			exports: {}
-// /******/ 		};
-// /******/
-// /******/ 		// Execute the module function
-// /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-// /******/
-// /******/ 		// Flag the module as loaded
-// /******/ 		module.l = true;
-// /******/
-// /******/ 		// Return the exports of the module
-// /******/ 		return module.exports;
-// /******/ 	}
-// /******/
-// /******/
-// /******/ 	// expose the modules object (__webpack_modules__)
-// /******/ 	__webpack_require__.m = modules;
-// /******/
-// /******/ 	// expose the module cache
-// /******/ 	__webpack_require__.c = installedModules;
-// /******/
-// /******/ 	// define getter function for harmony exports
-// /******/ 	__webpack_require__.d = function(exports, name, getter) {
-// /******/ 		if(!__webpack_require__.o(exports, name)) {
-// /******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-// /******/ 		}
-// /******/ 	};
-// /******/
-// /******/ 	// define __esModule on exports
-// /******/ 	__webpack_require__.r = function(exports) {
-// /******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-// /******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-// /******/ 		}
-// /******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-// /******/ 	};
-// /******/
-// /******/ 	// create a fake namespace object
-// /******/ 	// mode & 1: value is a module id, require it
-// /******/ 	// mode & 2: merge all properties of value into the ns
-// /******/ 	// mode & 4: return value when already ns object
-// /******/ 	// mode & 8|1: behave like require
-// /******/ 	__webpack_require__.t = function(value, mode) {
-// /******/ 		if(mode & 1) value = __webpack_require__(value);
-// /******/ 		if(mode & 8) return value;
-// /******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-// /******/ 		var ns = Object.create(null);
-// /******/ 		__webpack_require__.r(ns);
-// /******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-// /******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-// /******/ 		return ns;
-// /******/ 	};
-// /******/
-// /******/ 	// getDefaultExport function for compatibility with non-harmony modules
-// /******/ 	__webpack_require__.n = function(module) {
-// /******/ 		var getter = module && module.__esModule ?
-// /******/ 			function getDefault() { return module['default']; } :
-// /******/ 			function getModuleExports() { return module; };
-// /******/ 		__webpack_require__.d(getter, 'a', getter);
-// /******/ 		return getter;
-// /******/ 	};
-// /******/
-// /******/ 	// Object.prototype.hasOwnProperty.call
-// /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-// /******/
-// /******/ 	// __webpack_public_path__
-// /******/ 	__webpack_require__.p = "";
-// /******/
-// /******/
-// /******/ 	// Load entry module and return exports
-// /******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/main.js");
-// /******/ })
-// /************************************************************************/
-// /******/ ({
-
-// /***/ "./src/js/main.js":
-// /*!************************!*\
-//   !*** ./src/js/main.js ***!
-//   \************************/
-// /*! no static exports found */
-// /***/ (function(module, exports) {
-
-
-
-// /***/ })
-
-// /******/ });
-// //# sourceMappingURL=script.js.map
